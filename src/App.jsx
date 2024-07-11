bfef52f35abc6d2e91d0b75555390062b3733fda
Paul Allen <nickoconnor@gmail.com>
2024-07-11 23:21:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
